bafd6fa802e90fdcbcf43e703c7d6027848f16db
Samhita Alla <aallasamhita@gmail.com>
2022-05-05 14:08:43 +0530

    fix
    Signed-off-by: Samhita Alla <aallasamhita@gmail.com>

--- src/furo/theme/furo/static/scripts/algolia-search.js
+++ src/furo/theme/furo/static/scripts/algolia-search.js
@@ -5,7 +5,7 @@ docsearch({
   appId: "WLG0MZB58Q",
   apiKey: "28bf9bfd4a77a7d6b3ab7e98c671e781",
   indexName: "flyte",
-  algoliaOptions: {
+  searchParameters: {
     hitsPerPage: 10,
     // See https://www.algolia.com/doc/api-reference/api-parameters/
   },
